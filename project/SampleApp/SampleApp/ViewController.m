26e0d2d11ecea6f3a2dec08fa78a04bd95f3db87
杜承玖 <duchengjiu@qutoutiao.net>
2021-10-13 16:52:57 +0800

    创建一个UIView

--- project/SampleApp/SampleApp/ViewController.m
+++ project/SampleApp/SampleApp/ViewController.m
@@ -16,13 +16,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    [self.view addSubview:({
-        UILabel *label = [[UILabel alloc] init];
-        label.text = @"Hello";
-        [label sizeToFit];
-        label.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
-        label;
-    })];
+    
+    UIView *view = [[UIView alloc] init];
+    view.backgroundColor = [UIColor redColor];
+    view.frame = CGRectMake(150, 150, 100, 100);
+    [self.view addSubview:view];
+    
+    UIView *view2 = [[UIView alloc] init];
+    view2.backgroundColor = [UIColor greenColor];
+    view2.frame = CGRectMake(100, 100, 100, 100);
+    [self.view addSubview:view2];
 }
 
 
