5459010afd5a15232aaf939f716ff2393e5dbde1
杜承玖 <duchengjiu@qutoutiao.net>
2021-10-13 16:58:24 +0800

    UIView的生命周期

--- project/SampleApp/SampleApp/ViewController.m
+++ project/SampleApp/SampleApp/ViewController.m
@@ -7,6 +7,39 @@
 
 #import "ViewController.h"
 
+@interface TestView : UIView
+
+@end
+
+@implementation TestView
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+    }
+    return self;
+}
+//1
+- (void)willMoveToSuperview:(UIView *)newSuperview {
+    [super willMoveToSuperview:newSuperview];
+}
+//2
+- (void)didMoveToSuperview {
+    [super didMoveToSuperview];
+}
+//3
+- (void)willMoveToWindow:(UIWindow *)newWindow {
+    [super willMoveToWindow:newWindow];
+}
+//4
+- (void)didMoveToWindow {
+    [super didMoveToWindow];
+}
+
+@end
+
+
 @interface ViewController ()
 
 @end
@@ -15,17 +48,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    
-    UIView *view = [[UIView alloc] init];
+    self.view.backgroundColor = [UIColor whiteColor];
+
+    TestView *view = [[TestView alloc] init];
     view.backgroundColor = [UIColor redColor];
     view.frame = CGRectMake(150, 150, 100, 100);
     [self.view addSubview:view];
     
-    UIView *view2 = [[UIView alloc] init];
-    view2.backgroundColor = [UIColor greenColor];
-    view2.frame = CGRectMake(100, 100, 100, 100);
-    [self.view addSubview:view2];
+//    UIView *view2 = [[UIView alloc] init];
+//    view2.backgroundColor = [UIColor greenColor];
+//    view2.frame = CGRectMake(100, 100, 100, 100);
+//    [self.view addSubview:view2];
 }
 
 
