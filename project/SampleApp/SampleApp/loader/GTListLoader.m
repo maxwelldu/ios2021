3a42abe1c822621a0fa755d91aa0724070c284e6
杜承玖 <duchengjiu@qutoutiao.net>
2021-10-14 13:21:37 +0800

    使用cocoapod, 使用第一个第三方库AFNetworking

--- project/SampleApp/SampleApp/loader/GTListLoader.m
+++ project/SampleApp/SampleApp/loader/GTListLoader.m
@@ -6,6 +6,7 @@
 //
 
 #import "GTListLoader.h"
+#import <AFNetworking.h>
 
 @implementation GTListLoader
 
@@ -13,12 +14,21 @@
     NSString *urlString = @"http://is.snssdk.com/api/news/feed/v51/";
     // 去掉不使用的警告
     __unused NSURL *listURL = [NSURL URLWithString:urlString];
+    
+    [[AFHTTPSessionManager manager] GET:urlString parameters:nil headers:nil progress:^(NSProgress * _Nonnull downloadProgress) {
+            
+        } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            
+        }];
+    
 //    NSURLRequest *listRequest = [NSURLRequest requestWithURL:listURL];
-    NSURLSession *session = [NSURLSession sharedSession];
-    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:listURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        
-    }];
-    [dataTask resume];
+//    NSURLSession *session = [NSURLSession sharedSession];
+//    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:listURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+//
+//    }];
+//    [dataTask resume];
 }
 
 @end
