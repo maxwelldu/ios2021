e9ae89440e66d946a48bfc8ed5c1bb7660d6bbd1
杜承玖 <duchengjiu@qutoutiao.net>
2021-10-14 13:28:23 +0800

    json解析原生的数据

--- project/SampleApp/SampleApp/loader/GTListLoader.m
+++ project/SampleApp/SampleApp/loader/GTListLoader.m
@@ -15,20 +15,22 @@
     // 去掉不使用的警告
     __unused NSURL *listURL = [NSURL URLWithString:urlString];
     
-    [[AFHTTPSessionManager manager] GET:urlString parameters:nil headers:nil progress:^(NSProgress * _Nonnull downloadProgress) {
-            
-        } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-            
-        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-            
-        }];
-    
-//    NSURLRequest *listRequest = [NSURLRequest requestWithURL:listURL];
-//    NSURLSession *session = [NSURLSession sharedSession];
-//    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:listURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+//    [[AFHTTPSessionManager manager] GET:urlString parameters:nil headers:nil progress:^(NSProgress * _Nonnull downloadProgress) {
 //
-//    }];
-//    [dataTask resume];
+//        } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+//
+//        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+//
+//        }];
+    
+    NSURLRequest *listRequest = [NSURLRequest requestWithURL:listURL];
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:listURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSError *jsonError;
+        id jsonObj = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+        NSLog(@"");
+    }];
+    [dataTask resume];
 }
 
 @end
