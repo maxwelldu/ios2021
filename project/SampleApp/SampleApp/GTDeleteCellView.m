3e7800ab8972ce6fd4583ab3106422ad1fa62925
杜承玖 <duchengjiu@qutoutiao.net>
2021-10-14 11:17:13 +0800

    给自定义弹窗组件添加简单的动画

--- project/SampleApp/SampleApp/GTDeleteCellView.m
+++ project/SampleApp/SampleApp/GTDeleteCellView.m
@@ -32,7 +32,7 @@
         })];
         
         [self addSubview:({
-            _deleteButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
+            _deleteButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
             [_deleteButton addTarget:self action:@selector(_clickButton) forControlEvents:UIControlEventTouchUpInside];
             _deleteButton.backgroundColor = [UIColor blueColor];
             _deleteButton;
@@ -44,7 +44,19 @@
 // 此组件是独立的组件，将其放到window上面
 - (void)showDeleteView {
     [[UIApplication sharedApplication].keyWindow addSubview:self];
+    // 最简单的动画
+//    [UIView animateWithDuration:1.f animations:^{
+//        self.deleteButton.frame = CGRectMake((self.bounds.size.width-200)/2, (self.bounds.size.height-200)/2, 200, 200);
+//    }];
+    
+    // 相对较为复杂的
+    [UIView animateWithDuration:1.f delay:0.f usingSpringWithDamping:0.5 initialSpringVelocity:0.5 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        self.deleteButton.frame = CGRectMake((self.bounds.size.width-200)/2, (self.bounds.size.height-200)/2, 200, 200);
+    } completion:^(BOOL finished){
+        NSLog(@"");
+    }];
 }
+
 
 - (void)dismissDeleteView {
     [self removeFromSuperview];
