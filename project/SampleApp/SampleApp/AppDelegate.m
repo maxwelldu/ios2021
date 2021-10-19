1def585d5951d963d3ae8f5f39985a76349ca8e3
maxwelldu <dcj3sjt@126.com>
2021-10-19 11:26:26 +0800

    演示两种异常捕获的注册方法，其他的也类似

--- project/SampleApp/SampleApp/AppDelegate.m
+++ project/SampleApp/SampleApp/AppDelegate.m
@@ -13,6 +13,7 @@
 #import "GTStaticTest.h"
 #import <GTFramework/GTFrameworkClass.h>
 #import "GTMineViewController.h"
+#import <execinfo.h>
 
 @interface AppDelegate ()
 
@@ -49,7 +50,10 @@
 //    [[GTStaticTest alloc] init];
     
     //framework
-    [[GTFrameworkClass alloc] init];
+//    [[GTFrameworkClass alloc] init];
+    
+    [self _caughtException];
+//    [@[].mutableCopy addObject:nil]; //make a crash
     return YES;
 }
 
@@ -60,5 +64,34 @@
 
 - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
     return YES;
+}
+
+#pragma mark - crash
+-(void)_caughtException {
+    //NSException
+    NSSetUncaughtExceptionHandler(HandleNSException);
+    
+    //signal
+    signal(SIGABRT, SignalExceptionHandler);
+    signal(SIGILL, SignalExceptionHandler);
+    signal(SIGSEGV, SignalExceptionHandler);
+    signal(SIGFPE, SignalExceptionHandler);
+    signal(SIGBUS, SignalExceptionHandler);
+    signal(SIGPIPE, SignalExceptionHandler);
+}
+void SignalExceptionHandler(int signal) {
+    void* callstack[128];
+    int frames = backtrace(callstack, 128);
+    char **strs = backtrace_symbols(callstack, frames);
+    NSMutableArray *backtrace = [NSMutableArray arrayWithCapacity:frames];
+    for(int i=0;i<frames;i++) {
+        [backtrace addObject:[NSString stringWithUTF8String:strs[i]]];
+    }
+    free(strs);
+}
+void HandleNSException(NSException *exception) {
+    __unused NSString *reason = [exception reason];
+    __unused NSString *name= [exception name];
+    //存储Crash信息
 }
 @end
